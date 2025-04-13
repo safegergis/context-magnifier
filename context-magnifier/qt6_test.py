6d4fb98314b1890c41a703b8262ea1a7c2081bd7
Holman <joshua_holman@csu.fullerton.edu>
2025-04-12 23:18:13 -0700

    form scaffolding

--- context-magnifier/qt6_test.py
+++ context-magnifier/qt6_test.py
@@ -1,25 +1,43 @@
 import sys
 from PySide6.QtCore import Qt
-from PySide6.QtWidgets import QApplication, QMainWindow, QToolBar, QPushButton
+from PySide6.QtWidgets import (
+    QApplication, QMainWindow, QToolBar, QPushButton, QGridLayout, QWidget, QFormLayout,
+    QLabel,
+    QLineEdit
+)
 
-class MinimalTransparentWindow(QMainWindow):
+class TransparentWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        
         self.setWindowFlags(Qt.WindowType.FramelessWindowHint)
         self.setAttribute(Qt.WidgetAttribute.WA_TranslucentBackground)
+
+        central_widget = QWidget()
+        grid_layout = QGridLayout()
+        central_widget.setLayout(grid_layout)
+        self.setCentralWidget(central_widget)
+
+        # Configuration Form
         
-        toolbar = QToolBar()
+        test_label = QLabel("Test:")
+        test_input = QLineEdit()
+        test_input.setPlaceholderText("Enter Value")
+
+        grid_layout.addWidget(test_label, 1, 1)
+        grid_layout.addWidget(test_input, 1, 2)
+
+        # Toolbar
+        toolbar = QToolBar("Main Toolbar")
         self.addToolBar(toolbar)
-        
         close_button = QPushButton("Close")
         close_button.clicked.connect(self.close)
         toolbar.addWidget(close_button)
-        
+
         self.showFullScreen()
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    window = MinimalTransparentWindow()
+    window = TransparentWindow()
     window.show()
     sys.exit(app.exec())
