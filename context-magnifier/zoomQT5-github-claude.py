c5df413903bead34e14a714017d0c0784a05eb99
Luke Arcebal <103071251+LukeBArcebal@users.noreply.github.com>
2025-04-12 21:00:48 -0700

    removed uneccessary code

--- context-magnifier/zoomQT5-github-claude.py
+++ context-magnifier/zoomQT5-github-claude.py
@@ -13,7 +13,6 @@ class ScreenMagnifier(QWidget):
     def __init__(self):
         super().__init__()
         self.scale_factor = 2.5  # Default scale factor
-        self.zoom_increment = 0.1  # Zoom increment for each step
         
         # Set fixed dimensions for magnifier window
         self.window_width = 300
@@ -41,48 +40,10 @@ class ScreenMagnifier(QWidget):
         self.timer.timeout.connect(self.update_magnifier)
         self.timer.start(30)  # Update every 30 milliseconds
         
-        # Create a system tray icon
-        self.create_context_menu()
-        self.tray_icon = QSystemTrayIcon(self)
-        # Use fallback icon if icon.png is missing
-        try:
-            self.tray_icon.setIcon(QIcon("icon.png"))
-        except:
-            self.tray_icon.setIcon(QIcon.fromTheme("zoom-fit-best"))
-
-        # Set the context menu for the system tray icon
-        self.tray_icon.setContextMenu(self.tray_menu)
-        self.tray_icon.show()
-
     def update_source_dimensions(self):
         """Update source region dimensions based on scale factor"""
         self.source_width = int(self.window_width / self.scale_factor)
         self.source_height = int(self.window_height / self.scale_factor)
-
-    def create_context_menu(self):
-        # Create a context menu for the system tray icon
-        self.tray_menu = QMenu(self)
-        self.zoom_in_action = QAction("Zoom In      (Ctrl+Up)", self)
-        self.zoom_out_action = QAction("Zoom Out    (Ctrl+Down)", self)
-        self.hide_action = QAction("Hide            (Esc)", self)
-        self.unhide_action = QAction("Unhide", self)
-        self.exit_action = QAction("Exit", self)
-
-        # Connect actions to their respective slots
-        self.zoom_in_action.triggered.connect(self.zoom_in)
-        self.zoom_out_action.triggered.connect(self.zoom_out)
-        self.hide_action.triggered.connect(self.hide)
-        self.unhide_action.triggered.connect(self.show)
-        self.exit_action.triggered.connect(self.close)
-
-        # Add actions to the context menu
-        self.tray_menu.addAction(self.zoom_in_action)
-        self.tray_menu.addAction(self.zoom_out_action)
-        self.tray_menu.addSeparator()
-        self.tray_menu.addAction(self.hide_action) 
-        self.tray_menu.addAction(self.unhide_action) 
-        self.tray_menu.addSeparator()
-        self.tray_menu.addAction(self.exit_action)
 
     def update_magnifier(self):
         """Method for updating the window to follow cursor"""
@@ -93,13 +54,6 @@ class ScreenMagnifier(QWidget):
         window_x = mx + self.x_offset
         window_y = my + self.y_offset
         
-        # Check if the magnifier window would go offscreen and adjust if needed
-        screen_width, screen_height = pyautogui.size()
-        if window_x + self.window_width > screen_width:
-            window_x = mx - self.window_width - self.x_offset
-        if window_y + self.window_height > screen_height:
-            window_y = my - self.window_height - self.y_offset
-            
         # Move the window before taking the screenshot
         self.move(window_x, window_y)
         
@@ -141,32 +95,6 @@ class ScreenMagnifier(QWidget):
         pixmap = QPixmap.fromImage(qImg)
         self.label.setPixmap(pixmap)
 
-    def zoom_in(self):
-        """Method for zooming in"""
-        self.scale_factor = min(self.scale_factor + self.zoom_increment, 5)
-        self.update_source_dimensions()
-        
-    def zoom_out(self):
-        """Method for zooming out"""
-        self.scale_factor = max(1.1, self.scale_factor - self.zoom_increment)
-        self.update_source_dimensions()
-    
-    def keyPressEvent(self, event):
-        """Handles key events for the app"""        
-        # Ctrl + ...
-        if event.modifiers() == Qt.ControlModifier:
-            # zoom in (ctrl + up)
-            if event.key() == Qt.Key_Up:
-                self.zoom_in()
-                
-            # zoom out (ctrl + down)
-            elif event.key() == Qt.Key_Down:
-                self.zoom_out()
-        
-        # hide magnifier (Esc)
-        elif event.key() == Qt.Key_Escape:
-            self.hide()
-        
     def closeEvent(self, event):
         self.exit_signal.emit()
 
