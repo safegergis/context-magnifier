117a82526ca4dcaf4f9ea86c9d99395c5936a7de
Josh Holman <joshuaholman5@gmail.com>
2025-04-13 02:34:07 -0700

    fix names

--- context-magnifier/main.py
+++ context-magnifier/main.py
@@ -1,10 +1,10 @@
 import multiprocessing
-from app.main_window import run_main_window_sync
-from app.zoom_window import run_zoom_window_sync
+from app.main_window import run_main_window
+from app.zoom_window import run_zoom_window
 
 if __name__ == "__main__":
-    p1 = multiprocessing.Process(target=run_zoom_window_sync)
-    p2 = multiprocessing.Process(target=run_main_window_sync)
+    p1 = multiprocessing.Process(target=run_zoom_window)
+    p2 = multiprocessing.Process(target=run_main_window)
     
     p1.start()
     p2.start()
